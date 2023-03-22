a8af8f954cac6e9d4c3b8b29683cfb8214198756
Inkyagroup <113021894+Inkyagroup@users.noreply.github.com>
2023-03-22 19:11:55 +0900

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -13,3 +13,20 @@ function Music() {
     x++;
     console.log(x);
 }
+
+function mail(){
+    const formElements = document.forms.otoiawase;
+    console.log(formElements);
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "inkyagroup0603@gmail.com",
+        Password : "24EF1E8A23BB634D52E6CA7344D228134061",
+        To :"inkyagroup0603@gmail.com",
+        From : "inkyagroup0603@gmail.com",
+        Subject : formElements.title.value,
+        Body : formElements.name.value + formElements.gender.value + formElements.message.value
+    })
+    .then(
+        message => alert(message)
+    );
+}
